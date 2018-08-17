47ba140f38d467112b75401b392fe17d5df8a659
Michael Rocha <33291574+Essex09@users.noreply.github.com>
2018-08-17 14:09:43 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,20 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 # Update
+echo -e "Updating Kali"
 apt-get update -y
 
 # Install i3 and it's dependencies
+sleep .5;
+echo -e "\n"
+echo -e "Installing i3"
 apt-get install i3 -y
 
 # i3-gaps Dependencies
-
-apt-get install libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev
+sleep .5;
+echo -e "\n"
+echo -e "Installing i3-gaps dependencies"
+apt-get install -y libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev
 
 # Copy Custom configs
 #cd /root/
 #git clone https://github.com/Essex09/dotfiles.git
 
 # i3-gaps Installation
+sleep .5;
+echo -e "\n"
+echo -e "Installing i3-gaps"
 mkdir gui && cd gui
 git clone https://www.github.com/Airblader/i3 i3-gaps && cd i3-gaps
 
@@ -33,6 +42,9 @@ sudo make install
 ##### Kali Ricing
 # lxappearance changes themes and icons
 # nitrogen used to change wallpaper
+sleep .5;
+echo -e "\n"
+echo -e "Ricing Kali"
 apt-get install -y lxappearance nitrogen arc-theme moka-icon-theme rofi compton xfce4-terminal
 
 # ddnotes - Dependencies
@@ -45,3 +57,8 @@ make && make install
 ## ddnotes Installation
 cd /root/gui/
 git clone https://github.com/umurgdk/ddnotes.git && cp ddnotes/ddnotes /usr/bin/
+
+## Finish
+sleep .5;
+echo -e "\n"
+echo -e "Finished. Please restart to login to i3."
