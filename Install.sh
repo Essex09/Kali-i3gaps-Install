e503705781e6d4cc1b0d18d143900cfadfe9e04a
Michael Rocha <33291574+Essex09@users.noreply.github.com>
2018-09-25 10:42:48 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -6,20 +6,35 @@ apt-get update -y
 
 # Install i3 and it's dependencies
 sleep .5;
-echo -e "\n"
+echo -e "."
+echo -e "."
+echo -e "."
 echo -e "Installing i3"
+echo -e "."
+echo -e "."
+echo -e "."
 apt-get install i3 -y
 
 # i3-gaps Dependencies
 sleep .5;
-echo -e "\n"
+echo -e "."
+echo -e "."
+echo -e "."
 echo -e "Installing i3-gaps dependencies"
+echo -e "."
+echo -e "."
+echo -e "."
 apt-get install -y libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev
 
 # i3-gaps Installation
 sleep .5;
-echo -e "\n"
+echo -e "."
+echo -e "."
+echo -e "."
 echo -e "Installing i3-gaps"
+echo -e "."
+echo -e "."
+echo -e "."
 mkdir gui && cd gui
 git clone https://www.github.com/Airblader/i3 i3-gaps && cd i3-gaps
 
@@ -39,8 +54,13 @@ sudo make install
 # lxappearance changes themes and icons
 # nitrogen used to change wallpaper
 sleep .5;
-echo -e "\n"
+echo -e "."
+echo -e "."
+echo -e "."
 echo -e "Ricing Kali"
+echo -e "."
+echo -e "."
+echo -e "."
 apt-get install -y lxappearance nitrogen arc-theme moka-icon-theme rofi compton xfce4-terminal xfce4-panel papirus-icon-theme neofetch zathura
 
 # ddnotes - Dependencies
@@ -56,6 +76,8 @@ git clone https://github.com/umurgdk/ddnotes.git && cp ddnotes/ddnotes /usr/bin/
 
 ## Finish
 sleep .5;
-echo -e "\n"
+echo -e "."
+echo -e "."
+echo -e "."
 echo -e "Finished. Please restart to login to i3."
 echo -e "Download your own i3 config file to /root/.config/i3/"
