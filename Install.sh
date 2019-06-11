c69ecbd7acf6048552a677ac7ad85e46064084a0
Michael Rocha <33291574+Essex09@users.noreply.github.com>
2019-06-11 06:18:39 -0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,49 +1,42 @@
 #!/bin/bash
+# Author: Michael Rocha (@Essex09_)
+
 
 #Prevent shutter from being removed
 #echo "shutter hold" | sudo dpkg --set-selections
 
 # Update
-echo -e "."
-echo -e "."
-echo -e "."
-echo -e "Updating Kali"
-echo -e "."
-echo -e "."
-echo -e "."
+echo -e "###################"
+echo -e "#  Updating Kali  #" 
+echo -e "###################"
+
 apt-get update -y
 
 # Install i3 and it's dependencies
 sleep .5;
-echo -e "."
-echo -e "."
-echo -e "."
-echo -e "Installing i3"
-echo -e "."
-echo -e "."
-echo -e "."
+echo -e "\n"
+echo -e "###################"
+echo -e "#  Installing i3  #"
+echo -e "###################"
+
 apt-get install i3 -y
 
 # i3-gaps Dependencies
 sleep .5;
-echo -e "."
-echo -e "."
-echo -e "."
-echo -e "Installing i3-gaps dependencies"
-echo -e "."
-echo -e "."
-echo -e "."
+echo -e "\n"
+echo -e "#####################################"
+echo -e "#  Installing i3-gaps dependencies  #"
+echo -e "#####################################"
+
 apt-get install -y libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev
 
 # i3-gaps Installation
 sleep .5;
-echo -e "."
-echo -e "."
-echo -e "."
-echo -e "Installing i3-gaps"
-echo -e "."
-echo -e "."
-echo -e "."
+echo -e "\n"
+echo -e "########################"
+echo -e "#  Installing i3-gaps  #"
+echo -e "########################"
+
 mkdir gui && cd gui
 git clone https://www.github.com/Airblader/i3 i3-gaps && cd i3-gaps
 
@@ -63,18 +56,18 @@ sudo make install
 # lxappearance changes themes and icons
 # nitrogen used to change wallpaper
 sleep .5;
-echo -e "."
-echo -e "."
-echo -e "."
-echo -e "Ricing Kali"
-echo -e "."
-echo -e "."
-echo -e "."
+echo -e "\n"
+echo -e "#################"
+echo -e "#  Ricing Kali  #"
+echo -e "#################"
+
 apt-get install -y lxappearance nitrogen arc-theme moka-icon-theme rofi compton xfce4-terminal papirus-icon-theme neofetch zathura i3status
 
-echo -e "."
-echo -e "."
-echo -e "Adding Overpass font"
+echo -e "\n"
+echo -e "##########################"
+echo -e "#  Adding Overpass font  #"
+echo -e "##########################"
+
 git clone https://github.com/RedHatBrand/Overpass.git && cp Overpass/desktop-fonts/overpass-mono/overpass-mono-regular.otf /usr/share/fonts/
 fc-cache -fv
 
@@ -99,8 +92,8 @@ fc-cache -fv
 
 ## Finish
 sleep .5;
-echo -e "."
-echo -e "."
-echo -e "."
-echo -e "Finished. Please restart to login to i3."
-echo -e "Download your own i3 config file to /root/.config/i3/"
+echo -e "\n"
+echo -e "############################################################"
+echo -e "#  Finished. Please restart to login to i3.               #"
+echo -e "#  Download your own i3 config file to /root/.config/i3/  #"
+echo -e "############################################################"
